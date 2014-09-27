45b6f255f20a1a194c872e3065c6125ec93a0256
Moez Bouhlel <bmoez.j@gmail.com>
2014-09-27 16:43:01 +0100

    fix panel-control multie time adding btns

--- src/main.js
+++ src/main.js
@@ -39,15 +39,17 @@ config_r.onload = function() {
 			var style = document.createElement("style");
 			style.appendChild(document.createTextNode(""));
 			document.head.appendChild(style);
-			style.sheet.insertRule("#control-panel a { display: inline-block; margin: 5px; border: solid 1px; padding: 5px;}", 0);
-			style.sheet.insertRule("#control-panel a.clicked {background-color: green}", 0);
-			style.sheet.insertRule("#control-panel a.unclicked {background-color: red}", 0);
+			style.sheet.insertRule(".control-panel-btn { display: inline-block; margin: 5px;}", 0);
+			style.sheet.insertRule(".control-panel-btn a { line-height: 25px; padding: 5px; margin: 0; border: solid 1px;}", 0);
+			style.sheet.insertRule(".control-panel-btn a.clicked {background-color: green}", 0);
+			style.sheet.insertRule(".control-panel-btn a.unclicked {background-color: red}", 0);
 
 			$('body').bind('blog-post-loaded', function(){
+				if (! $("#control-panel")[0] ) return;
 				$.each($(".main-nav li a"), function(i, e) {
-					$('#control-panel').append("<a class='clicked' data-id='" + e.id + "'>#"+ e.id + "</a>");
+					$('#control-panel').append("<div class='control-panel-btn'><a class='clicked' data-id='" + e.id + "'>#"+ e.id + "</a></div>");
 				});
-				$("#control-panel a").click(function(){
+				$(".control-panel-btn a").click(function(){
 					var id = this.dataset["id"];
 					if (this.className === "clicked") {
 						this.className = "unclicked";
@@ -57,6 +59,7 @@ config_r.onload = function() {
 						$("#" + id).parent().show(300);
 					}
 				});
+				$("#control-panel").removeAttr("id");
 			});
 		});
 	});
