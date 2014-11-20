03402e39cefd6134b463edf9389605c2095cc923
Moez Bouhlel <bmoez.j@gmail.com>
2014-11-20 23:04:22 +0100

    remove extracting data within asyncGet

--- src/services/instagram.js
+++ src/services/instagram.js
@@ -27,6 +27,8 @@
             asyncGet(API_URL + "users/" + settings.user_id + "/?access_token=" + settings.access_token),
             asyncGet(API_URL + "users/" + settings.user_id + "/media/recent/?access_token=" + settings.access_token)
         ]).then(function (res) {
+            res[0] = res[0].data;
+            res[1] = res[1].data;
             nextId = res[1]["pagination"]["next_max_id"];
             return Promise.resolve({
                 "user": res[0],
