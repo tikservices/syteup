144024d70fc5885a588916db139c67304546e7cd
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-15 13:11:00 +0100

    update syteup revision

--- demo/syteup-modules.js
+++ demo/syteup-modules.js
@@ -95,6 +95,17 @@
     }
     exportPlugin({ setup: setup }, "gratipay_widget");
 }(window));(function (window) {
+    "use strict";
+    function setup(settings) {
+        var form = document.createElement("form");
+        form.action = "https://www.paypal.com/cgi-bin/webscr";
+        form.method = "post";
+        form.target = "_top";
+        form.innerHTML = "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">" + "<input type=\"hidden\" name=\"encrypted\" value=\"" + settings["encrypted"] + "\">" + "<input type=\"image\" src=\"https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\">" + "<img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\">";
+        document.getElementById("header-widgets").appendChild(form);
+    }
+    exportPlugin({ setup: setup }, "paypal_donate");
+}(window));(function (window) {
     /* global DISQUS */
     "use strict";
     function embedDisqus(settings) {
--- demo/syteup.js
+++ demo/syteup.js
@@ -191,82 +191,6 @@ window.syncGet = function(url, success, headers, failure) {
         });
     };
 }(window || global));"use strict";
-var postOffset, postsOpts;
-function setupBlog(settings) {
-    registerPostLinkClick(settings);
-    registerTagLinkClick(settings);
-    registerHomeItemClick(settings);
-    window.sharethis_enabled = settings["blogs_settings"]["plugins"]["sharethis"] || false;
-    if (settings["blogs_settings"]["plugins"]["disqus"])
-        importM("disqusPlugin", "plugins/disqus").then(function ($plugin) {
-            $plugin.setup(settings["plugins_settings"]["disqus"]);
-        });
-    window.reachedEnd = false;
-    if (location.hash.substr(0, 7) === "#!post/")
-        postsOpts = { id: location.hash.slice(7).split("#")[0] };
-    else if (location.hash.substr(0, 6) === "#!tag/")
-        postsOpts = { tag: location.hash.slice(6).split("#")[0] };
-    return fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-        postOffset = offset;
-        if (window.sharethis_enabled)
-            importM("sharethisPlugin", "plugins/sharethis").then(function ($plugin) {
-                $plugin.setup(settings["plugins_settings"]["sharethis"]);
-            });
-        return Promise.resolve();
-    });
-}
-function registerPostLinkClick(settings) {
-    $(document).on("click", ".post-link", function () {
-        if (this.dataset["id"]) {
-            postsOpts = { id: this.dataset["id"] };
-            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts);
-        }
-        postOffset = 0;
-    });
-}
-function registerTagLinkClick(settings) {
-    $(document).on("click", ".tag-link", function () {
-        if (this.textContent) {
-            window.reachedEnd = false;
-            postsOpts = { tag: this.textContent };
-            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-                postOffset = offset;
-            });
-        }
-    });
-}
-function registerHomeItemClick(settings) {
-    $("#home-item-link").on("click", function () {
-        if (location.hash.substr(0, 2) !== "#!")
-            return;
-        location.hash = "";
-        postsOpts = undefined;
-        window.reachedEnd = false;
-        fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"]).then(function (offset) {
-            postOffset = offset;
-        });
-    });
-}
-function registerScroll(settings) {
-    var resultsLoaded = false, scrollWait = false, scrollWaitDur = 250;
-    $(window).scroll(function () {
-        if (!window.reachedEnd && !resultsLoaded && !scrollWait && postOffset && $(window).scrollTop() + $(window).height() > $(document).height() / 1.2) {
-            resultsLoaded = true;
-            scrollWait = true;
-            fetchBlogPosts(postOffset, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-                postOffset = offset;
-                scrollWait = false;
-            });
-            // Only load posts at most every scrollWaitDur milliseconds.
-            setTimeout(function () {
-                scrollWait = false;
-            }, scrollWaitDur);
-        }
-        if (resultsLoaded && $(window).scrollTop() + $(window).height() < $(document).height() / 1.2) {
-            resultsLoaded = false;
-        }
-    });
-}"use strict";
 /** renderBlogPosts
      *
      * Takes the response from the blog platform and renders it using
@@ -379,6 +303,82 @@ function setupBlogHeaderScroll() {
             else
                 this.className = "current";
         });
+    });
+}"use strict";
+var postOffset, postsOpts;
+function setupBlog(settings) {
+    registerPostLinkClick(settings);
+    registerTagLinkClick(settings);
+    registerHomeItemClick(settings);
+    window.sharethis_enabled = settings["blogs_settings"]["plugins"]["sharethis"] || false;
+    if (settings["blogs_settings"]["plugins"]["disqus"])
+        importM("disqusPlugin", "plugins/disqus").then(function ($plugin) {
+            $plugin.setup(settings["plugins_settings"]["disqus"]);
+        });
+    window.reachedEnd = false;
+    if (location.hash.substr(0, 7) === "#!post/")
+        postsOpts = { id: location.hash.slice(7).split("#")[0] };
+    else if (location.hash.substr(0, 6) === "#!tag/")
+        postsOpts = { tag: location.hash.slice(6).split("#")[0] };
+    return fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
+        postOffset = offset;
+        if (window.sharethis_enabled)
+            importM("sharethisPlugin", "plugins/sharethis").then(function ($plugin) {
+                $plugin.setup(settings["plugins_settings"]["sharethis"]);
+            });
+        return Promise.resolve();
+    });
+}
+function registerPostLinkClick(settings) {
+    $(document).on("click", ".post-link", function () {
+        if (this.dataset["id"]) {
+            postsOpts = { id: this.dataset["id"] };
+            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts);
+        }
+        postOffset = 0;
+    });
+}
+function registerTagLinkClick(settings) {
+    $(document).on("click", ".tag-link", function () {
+        if (this.textContent) {
+            window.reachedEnd = false;
+            postsOpts = { tag: this.textContent };
+            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
+                postOffset = offset;
+            });
+        }
+    });
+}
+function registerHomeItemClick(settings) {
+    $("#home-item-link").on("click", function () {
+        if (location.hash.substr(0, 2) !== "#!")
+            return;
+        location.hash = "";
+        postsOpts = undefined;
+        window.reachedEnd = false;
+        fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"]).then(function (offset) {
+            postOffset = offset;
+        });
+    });
+}
+function registerScroll(settings) {
+    var resultsLoaded = false, scrollWait = false, scrollWaitDur = 250;
+    $(window).scroll(function () {
+        if (!window.reachedEnd && !resultsLoaded && !scrollWait && postOffset && $(window).scrollTop() + $(window).height() > $(document).height() / 1.2) {
+            resultsLoaded = true;
+            scrollWait = true;
+            fetchBlogPosts(postOffset, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
+                postOffset = offset;
+                scrollWait = false;
+            });
+            // Only load posts at most every scrollWaitDur milliseconds.
+            setTimeout(function () {
+                scrollWait = false;
+            }, scrollWaitDur);
+        }
+        if (resultsLoaded && $(window).scrollTop() + $(window).height() < $(document).height() / 1.2) {
+            resultsLoaded = false;
+        }
     });
 }"use strict";
 var allComponents = [], enabledServices = [];
